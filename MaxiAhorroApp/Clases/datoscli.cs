2f67105f30a6712c3cc60ea5002cf057b3a7b258
fabro00150 <154337141+fabro00150@users.noreply.github.com>
2024-08-06 20:44:37 -0500

    verificando si se subio

---
+++ MaxiAhorroApp/Clases/datoscli.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxiAhorroApp.Clases
+{
+    internal class datoscli
+    {
+        public int idcliente { get; set; }
+        public int numfactura { get; set; }
+        public string nombrecliente { get; set; }
+        public string cedulacliente { get; set; }  
+        public string direccioncliente { get; set; }
+        public string telefonocliente { get; set; }
+        public string formapago { get; set; }
+        public DateTime fechapago { get; set; }
+    }
+}
